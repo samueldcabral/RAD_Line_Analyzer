a4540d8395438d445177c6498fb7d20c1520ab9d
samuel <samueldcabral@gmail.com>
2020-09-22 03:13:44 -0300

    final adjustments in the logic

--- LineAnalyzer.rb
+++ LineAnalyzer.rb
@@ -1,7 +1,6 @@
 class LineAnalyzer
 
-  @most_words = []
-
+  @most_words = Array.new
   attr_accessor :line_number, :content, :most_words
 
   def initialize(num, cont)
@@ -9,12 +8,46 @@ class LineAnalyzer
     @content = cont
   end
 
-  def analyze_frequency
+  def analyze_frequency(my_algo = false)
+    most_words_temp = Array.new
     content_arr = @content.split
+    
+    #providing true in my_algo will use my own algorithm for tallying the word occurrences, instead of using built-in method tally.
+    if(my_algo)
+      #My own algorithm
+      max_occurrence_tally = tally_ocurrences(content_arr)
+    else
+      #Tally method tallies the word ocurrences in the content_arr and saves it in a hash
+      max_occurrence_tally = content_arr.tally
+    end
 
-    content_arr.each{ |value|  
-      p value
+    #Grab the max number of ocurrencies and stores it in a variable
+    max_occurrence_num = max_occurrence_tally.values.max 
+
+    #Push the word or words that match the max number of occurrences
+    content_arr.tally.each{ |key, value| 
+      if value == max_occurrence_num 
+        most_words_temp.push(key)
+      end
     }
+
+    #Just to save it in the class attr
+    @most_words = most_words_temp
+
+    #tenary operator just to make it pretty
+    word_or_words = @most_words.length > 1 ? "words" : "word"
+    is_are = @most_words.length > 1 ? "are" : "is"
+
+    p "The most frequent #{word_or_words} in line #{@line_number} #{is_are} [#{@most_words.join(" - ")}] with #{max_occurrence_num} occurrences"
   end
 
+  def tally_ocurrences(content_arr)
+    seen = Hash.new
+
+    content_arr.each{ |value|
+      seen.has_key?(value) ? seen.store(value, seen[value] += 1) : seen[value] = 1
+    }
+
+    return seen
+  end
 end
--- main.rb
+++ main.rb
@@ -2,19 +2,16 @@ require_relative 'LineAnalyzer'
 
 my_file = File.open("ArquivoDeTextoGenerico.txt", "r")
 
+#Array to store all instances of the Class
 analyzers_arr = Array.new
 
 my_file.each_with_index { |value, idx| 
-  
-  if value.length > 1 #check if line isnt blank
+  if value != "\n" #skip blank lines
     analyzers_arr.push(LineAnalyzer.new(idx+1, value))
   end
 }
 
-analyzers_arr.each {
-  |value|
-
-  if value.line_number == 1
-    value.analyze_frequency
-  end
+analyzers_arr.each { |value|
+  value.analyze_frequency(true) #providing true will use my own algorithm for tallying the word occurrences, instead of using built-in method tally.
+  # value.analyze_frequency
 }
